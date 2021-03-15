c62c32f9a0bf47d0b1cc2a76b7874e690378b832
Vlad4496 <vlad_4496@yahoo.com>
2021-03-15 17:58:15 +0200

    componentsSpliting

---
+++ src/books.js
@@ -0,0 +1,44 @@
+export const books = [
+  {
+    id: 1,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL200_SR200,200_.jpg",
+    title: "It's Not Easy Being a Bunny",
+    author: "Marilyn Sadler",
+  },
+  {
+    id: 2,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/618LsqHnfgL._AC_UL200_SR200,200_.jpg",
+    title: "How to Catch a Leprechaun",
+    author: "Adam Wallace",
+  },
+  {
+    id: 3,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+    title: "I love you to the Moon and Back",
+    author: "Amelia Hepworth",
+  },
+  {
+    id: 4,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/91g%2BaK0nCnL._AC_UL200_SR200,200_.jpg",
+    title: "The Four Winds: A Novel",
+    author: "Kristin Hannah",
+  },
+  {
+    id: 5,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
+    title: "If Animals Kissed Good Night",
+    author: "Ann Whitford Paul",
+  },
+  {
+    id: 6,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/81YzHKeWq7L._AC_UL200_SR200,200_.jpg",
+    title: "The Midnight Library: A Novel",
+    author: "Matt Haig",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,65 +1,17 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-
-const books = [
-  {
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL200_SR200,200_.jpg",
-    title: "It's Not Easy Being a Bunny",
-    author: "Marilyn Sadler",
-  },
-  {
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/618LsqHnfgL._AC_UL200_SR200,200_.jpg",
-    title: "How to Catch a Leprechaun",
-    author: "Adam Wallace",
-  },
-  {
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-    title: "I love you to the Moon and Back",
-    author: "Amelia Hepworth",
-  },
-  {
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/91g%2BaK0nCnL._AC_UL200_SR200,200_.jpg",
-    title: "The Four Winds: A Novel",
-    author: "Kristin Hannah",
-  },
-  {
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
-    title: "If Animals Kissed Good Night",
-    author: "Ann Whitford Paul",
-  },
-  {
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/81YzHKeWq7L._AC_UL200_SR200,200_.jpg",
-    title: "The Midnight Library: A Novel",
-    author: "Matt Haig",
-  },
-];
+import { books } from "./books";
+import Book from "./Book";
 
 function BookList() {
   return (
     <section className="bookList">
-      {books.map((book, index) => {
-        return <Book book={book} key={index} />;
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
       })}
     </section>
   );
 }
-
-const Book = (props) => {
-  const { img, title, author } = props.book;
-  return (
-    <article className="book">
-      <img src={img} alt="Book for Kids" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-    </article>
-  );
-};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
