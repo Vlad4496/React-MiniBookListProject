2444e82534ce09f7a10fb8643b5bbc58b5cc6734
Vlad4496 <vlad_4496@yahoo.com>
2021-03-15 16:19:49 +0200

    commit.props

--- src/index.js
+++ src/index.js
@@ -2,35 +2,89 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
+const firstBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL200_SR200,200_.jpg",
+  title: "It's Not Easy Being a Bunny",
+  author: "Marilyn Sadler",
+};
+const secondBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/618LsqHnfgL._AC_UL200_SR200,200_.jpg",
+  title: "How to Catch a Leprechaun",
+  author: "Adam Wallace",
+};
+const thirdBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+  title: "I love you to the Moon and Back",
+  author: "Amelia Hepworth",
+};
+const fourthBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/91g%2BaK0nCnL._AC_UL200_SR200,200_.jpg",
+  title: "The Four Winds: A Novel",
+  author: "Kristin Hannah",
+};
+const fifthBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
+  title: "If Animals Kissed Good Night",
+  author: "Ann Whitford Paul",
+};
+const sixthBook = {
+  img:
+    "https://images-na.ssl-images-amazon.com/images/I/81YzHKeWq7L._AC_UL200_SR200,200_.jpg",
+  title: "The Midnight Library: A Novel",
+  author: "Matt Haig",
+};
+
 function BookList() {
   return (
     <section className="bookList">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+      <Book
+        img={thirdBook.img}
+        title={thirdBook.title}
+        author={thirdBook.author}
+      />
+      <Book
+        img={fourthBook.img}
+        title={fourthBook.title}
+        author={fourthBook.author}
+      />
+      <Book
+        img={fifthBook.img}
+        title={fifthBook.title}
+        author={fifthBook.author}
+      />
+      <Book
+        img={sixthBook.img}
+        title={sixthBook.title}
+        author={sixthBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
   return (
     <article className="book">
-      <Image></Image>
-      <Title></Title>
-      <Author></Author>
+      <img src={img} alt="Book for Kids" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL200_SR200,200_.jpg"
-    alt=""
-  />
-);
-const Title = () => <h1>It's Not Easy Being a Bunny (Beginner Books(R))</h1>;
-const Author = () => <h4>Marilyn Sadler</h4>;
 
 ReactDom.render(<BookList />, document.getElementById("root"));
